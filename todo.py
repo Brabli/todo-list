5b7fa5675a3fe0d942f4a281f051c201c2cb2819
Brabli <67018167+Brabli@users.noreply.github.com>
2021-05-23 15:56:15 +0100

    Create gitignore

--- todo.py
+++ todo.py
@@ -1,1 +1,22 @@
 #!/usr/local/bin/python3
+import os
+import sys
+from pathlib import Path
+
+
+
+filepath = sys.argv[0]
+args = sys.argv[1:]
+
+print(filepath)
+print(args)
+
+#sys.exit()
+
+def create_todo_list():
+    todo_list = Path("./todo_list.txt")
+    if not todo_list.exists():
+        print("Initialising todo list.")
+        file = open("todo_list.txt", "w")
+        file.write("TODO LIST\n################")
+        file.close("todo_list.txt")
