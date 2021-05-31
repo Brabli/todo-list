9955a34b4b9ad51d756376f1b44040d071d2e7c8
Brabli <67018167+Brabli@users.noreply.github.com>
2021-05-31 23:07:53 +0100

    Add cleanup_list function

--- todo.py
+++ todo.py
@@ -7,6 +7,7 @@
 #################
 import os
 import sys
+import argparse
 from pathlib import Path
 
 
@@ -15,16 +16,25 @@ from pathlib import Path
 #################
 current_script_path = os.path.dirname(os.path.realpath(__file__))
 
-if current_script_path == "/Users/bradley/Desktop/Personal Projects/todo":
-    todo_list_path = "./todo_list.txt"
-else:
-    todo_list_path = "/Users/bradley/bin/todo_list.txt"
+# parser = argparse.ArgumentParser()
+# parser.parse_args()
+
+todo_list_path = "./todo_list.txt" if current_script_path == "/Users/bradley/Desktop/Personal Projects/todo" else "/Users/bradley/bin/todo_list.txt"
 
 all_args = sys.argv[1:]
 
 #################
 ### FUNCTIONS ###
 #################
+
+def cleanup_list():
+    with open(todo_list_path, 'r') as todo_list:
+        all_items = todo_list.readlines()
+    with open(todo_list_path, "w") as todo_list:
+        for line in all_items:
+            if line.strip():
+                todo_list.write(line)
+
 def create_todo_list():
     todo_list = Path(todo_list_path)
     if not todo_list.exists():
@@ -32,10 +42,10 @@ def create_todo_list():
         file.close()
 
 # args = array of strings to join
-# This can be run after parse_options as all_args array will be updated
-def create_list_item(args):
+def create_list_item(all_args):
+    item_args = list(filter(lambda item : item[0] != "-", all_args))
     with open(todo_list_path, "a") as todo_list:
-        todo_item = " ".join(args)
+        todo_item = " ".join(item_args)
         todo_list.write(todo_item + "\n")
 
 # list_item = string, list item
@@ -53,13 +63,11 @@ def show_todo_list():
         print("\n")
 
 # args = array, global all_args
-# Side effect: Removes option from all_args array.
 def get_options(args):
     options = []
     for arg in args:
         if arg[0] == "-":
             options.append(arg);
-            args.pop(0)
         else:
             break
     return options
@@ -69,17 +77,19 @@ def remove_item(item_number):
     item_index = item_number - 1
     with open(todo_list_path, "r") as todo_list:
         all_items = todo_list.readlines()
+        #filtered_items = list(filter(lambda item : item.strip() != "", all_items))
     with open(todo_list_path, "w") as todo_list:
         for i, item in enumerate(all_items):
-            if i != item_index:
+            if i != item_index and item.strip() != "":
                 todo_list.write(item)
 
 # options = array, command options
-def parse_options(all_args):
+# options needs to be ["F", [12, 42, 5]] for example
+# Make it so having multiple remove flags doesn't break shit
+def parse_options(options):
     try:
-        for option in all_args:
+        for option in options:
             letter = option[1]
-            # r = REMOVE
             if letter == "r":
                 line_number = int(option[2:])
                 remove_item(line_number)        
@@ -97,13 +107,13 @@ def parse_options(all_args):
 #################
 
 create_todo_list()
+cleanup_list()
 
 if (len(all_args) == 0):
     show_todo_list()
     sys.exit()
 
 options = get_options(all_args)
-
 parse_options(options)
 
 if (len(all_args) > 0):
