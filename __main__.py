862cc72a009c6641f2c1b08dc37bd698745f3ae7
Brabli <67018167+Brabli@users.noreply.github.com>
2021-07-18 21:25:31 +0100

    Add comments

--- __main__.py
+++ __main__.py
@@ -3,12 +3,13 @@ from src.Parser import Parser
 from src.TodoList import TodoList
 
 def main():
+    # Pass all cli arguments (excluding the first arg which is always a filepath)
+    # into the parser to turn them into a dictionary.
     args = Parser.parse_args(sys.argv[1:])
     TodoList.execute_args(args)
     TodoList.clean()
     TodoList.show()
     sys.exit()
-
 
 if __name__ == '__main__':
   main()
