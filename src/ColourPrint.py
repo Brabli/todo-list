694e67c643b45eb40cec8e3c839db382e4f151c9
Brabli <67018167+Brabli@users.noreply.github.com>
2021-07-19 16:34:00 +0100

    Add methods to colourprint class

--- src/ColourPrint.py
+++ src/ColourPrint.py
@@ -1,6 +1,16 @@
 class ColourPrint:
+    colours = {
+        "red": "\33[91m",
+        "green": "\33[92m",
+        "orange": "\33[93m",
+        "blue": "\33[94m",
+        "purple": "\33[95m",
+        "cyan": "\33[96m"
+    }
+    end_colour = "\033[0m"
+
     @classmethod
-    def test_colours() -> None:
+    def __test_colours(cls) -> None:
         x = 0
         for i in range(24):
             colors = ""
@@ -9,3 +19,10 @@ class ColourPrint:
                 colors = colors + "\33[" + code + "m\\33[" + code + "m\033[0m "
             print(colors)
             x = x + 5
+
+    @classmethod
+    def print(cls, color: str, message: str) -> None:
+        colour_code = cls.colours[color];
+        end_colour = cls.end_colour
+        coloured_string = colour_code + message + end_colour
+        print(coloured_string)
