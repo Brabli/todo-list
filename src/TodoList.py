0c3b49588cf0259fb3eacf6cb5af754ad7159424
Brabli <67018167+Brabli@users.noreply.github.com>
2021-07-19 14:00:51 +0100

    Add new private method, cleanup code

--- src/TodoList.py
+++ src/TodoList.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from typing import List
 from .HistoryList import HistoryList
 
 class TodoList:
@@ -31,18 +32,16 @@ class TodoList:
 
     @classmethod
     def show(cls):
-        with open(cls.todo_list_path, "r") as todo_list:
-            print("\n#################\n### TODO LIST ###\n#################\n")
-            all_items = todo_list.readlines()
-            for i in range(len(all_items)):
-                formatted_item = (str(i + 1) + ". " + all_items[i]).strip()
-                print(formatted_item)
-            print("\n")
+        all_items = cls.__get_all_items()
+        print("\n#################\n### TODO LIST ###\n#################\n")
+        for i in range(len(all_items)):
+            formatted_item = (str(i + 1) + ". " + all_items[i]).strip()
+            print(formatted_item)
+        print("\n")
 
     @classmethod
     def clean(cls):
-        with open(cls.todo_list_path, 'r') as todo_list:
-            all_items = todo_list.readlines()
+        all_items = cls.__get_all_items()
         with open(cls.todo_list_path, "w") as todo_list:
             for line in all_items:
                 if line.strip():
@@ -64,9 +63,7 @@ class TodoList:
         :param item_index: Int, item index to add to list.
         :param amended_item: String, string to replace the current item.
         """
-        with open(cls.todo_list_path, "r") as todo_list:
-            all_items = todo_list.readlines()
-
+        all_items = cls.__get_all_items()
         with open(cls.todo_list_path, "w") as todo_list:
             for i, current_item in enumerate(all_items):
                 if (i != item_index):
@@ -78,19 +75,22 @@ class TodoList:
 
 
     @classmethod
-    def remove(cls, item_numbers):
+    def remove(cls, indicies_to_remove):
         """
         :param item_number: Int, number of line to be removed.
         """
-        for item_number in item_numbers:
-            item_index = item_number
-            with open(cls.todo_list_path, "r") as todo_list:
-                all_items = todo_list.readlines()
-
-            with open(cls.todo_list_path, "w") as todo_list:
+        all_items = cls.__get_all_items()
+        with open(cls.todo_list_path, "w") as todo_list:
+            for item_index in indicies_to_remove:
                 for i, item in enumerate(all_items):
                     if i != item_index and item.strip() != "":
                         todo_list.write(item)
                     else:
                         print(f"\nRemoved item {i + 1}: {item.strip()}")
                         HistoryList.add(item.strip(), "REMOVED")
+
+    @classmethod
+    def __get_all_items(cls) -> List[str]:
+        with open(cls.todo_list_path, "r") as todo_list:
+            all_items = todo_list.readlines()
+            return all_items
