bc86da7b55a6f2fc120f02367a3e0ffc404da19b
Brabli <67018167+Brabli@users.noreply.github.com>
2021-07-20 20:55:39 +0100

    Add multicolour method, change class name

--- src/ColourString.py
+++ src/ColourString.py
@@ -1,16 +1,21 @@
-class ColourPrint:
+from typing import Dict
+
+
+class ColourString:
     colours = {
         "red": "\33[91m",
         "green": "\33[92m",
         "orange": "\33[93m",
         "blue": "\33[94m",
         "purple": "\33[95m",
-        "cyan": "\33[96m"
+        "cyan": "\33[96m",
+        "white": "\33[37m",
+        "none": ""
     }
     end_colour = "\033[0m"
 
     @classmethod
-    def __test_colours(cls) -> None:
+    def test_colours(cls) -> None:
         x = 0
         for i in range(24):
             colors = ""
@@ -21,8 +26,15 @@ class ColourPrint:
             x = x + 5
 
     @classmethod
-    def print(cls, color: str, message: str) -> None:
-        colour_code = cls.colours[color];
+    def colour(cls, color: str, message: str) -> str:
+        colour_code = cls.colours.get(color, "none");
         end_colour = cls.end_colour
         coloured_string = colour_code + message + end_colour
-        print(coloured_string)
+        return coloured_string
+
+    @classmethod
+    def multicolour(cls, format_dict: Dict):
+        final_string = ""
+        for colour, string in format_dict.items():
+            final_string += cls.colour(colour, string)
+        return final_string
